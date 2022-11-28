0ff8109d1c6defc99f6eda2c44889105703291bf
Nat-Ntw <nattawong.sir@gmail.com>
2022-11-28 23:43:47 +0700

    chore:demo login page

--- src/App/Router.jsx
+++ src/App/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DashBoardPage from '../pages/DashBoard';
 import LandingPage from '../pages/LandingPage';
 import RegisterPage from '../pages/RegisterPage';
+import LoginPage from '../pages/LoginPage';
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashBoardPage />} />
       </Routes>
     </BrowserRouter>
