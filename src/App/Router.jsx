fc7cfcfb575817f7920abd04b44d6f1fb2e69618
Pavit Pimchanagul <ppimchan40@gmail.com>
2022-11-28 23:09:07 +0700

    chore:setup tailwind and demo route for new page

--- src/App/Router.jsx
+++ src/App/Router.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DashBoardPage from '../pages/DashBoard';
 import LandingPage from '../pages/LandingPage';
+import RegisterPage from '../pages/RegisterPage';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashBoardPage />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,9 @@
+function RegisterPage() {
+  return (
+    <h1 className="text-3xl font-bold ">
+      Hello Register
+    </h1>
+  );
+}
+
+export default RegisterPage;
