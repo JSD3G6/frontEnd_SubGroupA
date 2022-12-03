a4045448153f9aeb7d99c51b405f5a2c036cc63a
Pavit Pimchanagul <pavitpim@gmail.com>
2022-12-03 13:52:04 +0700

    feat:ui-register-desktop

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
 import BackgroundImage from '../assets/bg-1.png';
+import LogoAuth from '../assets/logo-auth.png';
 
 function RegisterPage() {
   const backgroundStyle = {
@@ -9,7 +11,27 @@ function RegisterPage() {
     backgroundSize: 'cover',
   };
   return (
-    <div style={backgroundStyle} />
+    <div className="flex justify-center items-center" style={backgroundStyle}>
+      <div className="w-[900px] h-[780px] rounded-[10px] bg-gray-primary px-[100px] flex flex-col">
+        <div className="mx-auto text-center pt-8">
+          <div className="mb-1">
+            <img src={LogoAuth} alt="logo-auth" />
+          </div>
+          <h3 className="font-thin text-[40px] text-white">Register</h3>
+        </div>
+
+        <form className="h-full flex flex-col justify-around ">
+          <div className="flex justify-between gap-4">
+            <input type="text" placeholder="First name" className="input-primary flex-1" />
+            <input type="text" placeholder="Last name" className="input-primary flex-1" />
+          </div>
+          <input type="text" placeholder="Email" className="input-primary" />
+          <input type="password" placeholder="Password" className="input-primary" />
+          <input type="password" placeholder="Confirm Password" className="input-primary" />
+          <button type="submit" className="btn-primary self-center">Next</button>
+        </form>
+      </div>
+    </div>
 
   );
 }
